4c31cda44fe1bd67426f821cae0b74a160a387ff
Pedro Soeiro <jpsoeirolemos@gmail.com>
2025-04-13 23:23:31 -0300

    primeira atualizacao

--- src/server.js
+++ src/server.js
@@ -2,17 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const teams = require("../dados/teams.json");
-const players = require("../dados/players.json");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/api/teams", (req, res) => {
   res.json({ teams });
-});
-
-app.get("/api/players", (req, res) => {
-  res.json({ players });
 });
 
 app.listen(3000, () => {
